8a55c25cd215c91e4f4733b19363cfcc781c0047
t32k <ijok.ijok@gmail.com>
2013-02-05 23:49:18 +0900

    Update v0.2
    Signed-off-by: t32k <ijok.ijok@gmail.com>

--- README.md
+++ README.md
@@ -4,4 +4,5 @@ This is a Chrome Extensions.
 
 ## Release History
 
++ 2013/02/06 - v0.2.0 - Public release.
 + 2013/02/05 - v0.1.0 - Initial release.
--- extensions.js
+++ extensions.js
@@ -1,17 +1,33 @@
+var sync  = chrome.storage.sync,
+	paths = [];
+
 $('.title').each(function(){
 
 	var $title = $(this),
-		path = $title.find('a').eq(0).attr('href');
+		path = $title.find('a').eq(0).attr('href'),
+		pathKey = path.replace(/\//, ''),
+		storageData = {};
 
-	$.ajax({
-		method: 'GET',
-		url: path,
-		dataType: 'html'
-	}).done(function(data) {
-		var avatarSrc = $(data).find('.avatared img').attr('src');
-		var avatarUrls = avatarSrc.replace('s=420', 's=140');
-		var $avatar = $('<img src="'+ avatarUrls + '" width="20" height="20" class="avatar" alt="">');
-		$title.before($avatar);
-	});
-	
+	// 重複アカウントチェックchrome.storage参照
+	if (_.include(paths, path)) {
+		sync.get(pathKey, function(val) {
+			var $avatar = $('<img src="'+ val[pathKey] + '" width="20" height="20" class="avatar" alt="">');
+			$title.before($avatar);
+		});
+	} else {
+		$.ajax({
+			method: 'GET',
+			url: path,
+			dataType: 'html'
+		}).done(function(data) {
+			var avatarSrc = $(data).find('.avatared img').attr('src');
+			var avatarUrl = avatarSrc.replace('s=420', 's=140');
+			var $avatar = $('<img src="'+ avatarUrl + '" width="20" height="20" class="avatar" alt="">');
+			$title.before($avatar);
+			// chrome.storagesにpathを貯めとく
+			storageData[pathKey] = avatarUrl;
+			sync.set(storageData);
+		});
+	}
+	paths.push(path);
 });
