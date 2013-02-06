f0ea879b4044fd56e45a63755e2e4fc8b5799c58
A12930 <A12930@pc-7851.cyberagent.co.jp>
2013-02-06 09:11:20 +0900

    Update v0.3.0

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# gFace
+# gFaces
 
-This is a Chrome Extensions.
+This is a Chrome Extensions for [GitHub News Feed](https://github.com/).
+
+## Dependencies
+
++ [jQuery](http://jquery.com/) 
++ [Underscore.js](http://underscorejs.org/)
 
 ## Release History
 
++ 2013/02/06 - v0.3.0 - Fix a bug load more.
 + 2013/02/06 - v0.2.0 - Public release.
 + 2013/02/05 - v0.1.0 - Initial release.
--- extensions.js
+++ extensions.js
@@ -1,33 +1,43 @@
 var sync  = chrome.storage.sync,
 	paths = [];
 
-$('.title').each(function(){
+function displayAvatar () {
+	$('.title').each(function(){
 
-	var $title = $(this),
-		path = $title.find('a').eq(0).attr('href'),
-		pathKey = path.replace(/\//, ''),
-		storageData = {};
+		var $title = $(this),
+			path = $title.find('a').eq(0).attr('href'),
+			pathKey = path.replace(/\//, ''),
+			storageData = {};
 
-	// 重複アカウントチェックchrome.storage参照
-	if (_.include(paths, path)) {
-		sync.get(pathKey, function(val) {
-			var $avatar = $('<img src="'+ val[pathKey] + '" width="20" height="20" class="avatar" alt="">');
-			$title.before($avatar);
-		});
-	} else {
-		$.ajax({
-			method: 'GET',
-			url: path,
-			dataType: 'html'
-		}).done(function(data) {
-			var avatarSrc = $(data).find('.avatared img').attr('src');
-			var avatarUrl = avatarSrc.replace('s=420', 's=140');
-			var $avatar = $('<img src="'+ avatarUrl + '" width="20" height="20" class="avatar" alt="">');
-			$title.before($avatar);
-			// chrome.storagesにpathを貯めとく
-			storageData[pathKey] = avatarUrl;
-			sync.set(storageData);
-		});
-	}
-	paths.push(path);
+		// 重複アカウントチェックchrome.storage参照
+		if (_.include(paths, path)) {
+			sync.get(pathKey, function(val) {
+				var $avatar = $('<img src="'+ val[pathKey] + '" width="20" height="20" class="avatar" alt="">');
+				$title.before($avatar);
+			});
+		} else {
+			$.ajax({
+				method: 'GET',
+				url: path,
+				dataType: 'html'
+			}).done(function(data) {
+				var avatarSrc = $(data).find('.avatared img').attr('src');
+				var avatarUrl = avatarSrc.replace('s=420', 's=140');
+				var $avatar = $('<img src="'+ avatarUrl + '" width="20" height="20" class="avatar" alt="">');
+				$title.before($avatar);
+				// chrome.storagesにpathを貯めとく
+				storageData[pathKey] = avatarUrl;
+				sync.set(storageData);
+			});
+		}
+		paths.push(path);
+	});
+}
+
+displayAvatar();
+
+$('.news').on('click', '.ajax_paginate > a', function() {
+	setTimeout(function() {
+		displayAvatar();
+	}, 500);
 });
